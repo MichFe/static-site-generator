40d8c37d4fba9b2a9db5e19d14a08a2b99a648d7
Michelle Felix <mich.feloz@gmail.com>
2025-01-23 14:44:52 -0600

    feat(splitter): Adding splitter
    Adding splitter function to project utils

---
+++ src/utils/splitter.py
@@ -0,0 +1,36 @@
+from src.classes.textnode import TEXT_TYPE, TextNode
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        node_text = old_node.text
+        new_nodes += split_text(node_text, delimiter, text_type)
+
+    print(f"Given old_nodes: {old_nodes}, delimiter: {delimiter}, text_type: {text_type}")
+    print(f"new_nodes: {new_nodes}")
+    return new_nodes
+
+
+def split_text(text, delimiter, text_type):
+    splitted = text.split(delimiter)
+    new_nodes = []
+
+    for i, item in enumerate(splitted):
+        if item == '':
+            continue
+
+        is_text = i % 2 == 0
+
+        new_node = None
+        if is_text:
+            new_node = TextNode(item, TEXT_TYPE.TEXT, None)
+        else:
+            new_node = TextNode(item, text_type, None)
+
+        new_nodes.append(new_node)
+    return new_nodes
+
+
+
+
