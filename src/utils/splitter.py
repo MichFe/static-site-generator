a4ff48085304135076783532c524a94d78881bf6
Michelle Felix <mich.feloz@gmail.com>
2025-01-24 18:44:22 -0600

    feat(splitter): Adding malformed md check and no delimiter check

--- src/utils/splitter.py
+++ src/utils/splitter.py
@@ -7,15 +7,20 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         node_text = old_node.text
         new_nodes += split_text(node_text, delimiter, text_type)
 
-    print(f"Given old_nodes: {old_nodes}, delimiter: {delimiter}, text_type: {text_type}")
-    print(f"new_nodes: {new_nodes}")
     return new_nodes
 
 
 def split_text(text, delimiter, text_type):
-    splitted = text.split(delimiter)
-    new_nodes = []
+    if not delimiter:
+        raise ValueError("A delimiter is required")
 
+    # The splitted array must be a multiple of 3, if not, the markdown is malformed
+    splitted = text.split(delimiter)
+    is_md_malformed = len(splitted) % 2 == 0 or len(splitted) < 3
+    if is_md_malformed:
+        raise ValueError("Markdown is malformed")
+
+    new_nodes = []
     for i, item in enumerate(splitted):
         is_text = i % 2 == 0
 
