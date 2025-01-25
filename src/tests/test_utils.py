f0cdce79c009b0cef609472b838baf1e6a043bb3
Michelle Felix <mich.feloz@gmail.com>
2025-01-24 18:43:49 -0600

    feat(splitter): Adding more splitter test cases
    Adding malformed md check, no delimiter check and multiple char delimiter check

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -16,7 +16,7 @@ class TestUtils(unittest.TestCase):
         self.assertListEqual(new_nodes, expected_result)
 
     def test_splitter_multiple(self):
-        node = TextNode("This is `code a` and this is `code b` and this is `code c`, tremendo", TEXT_TYPE.TEXT)
+        node = TextNode("This is `code a` and this is `code b` and this is `code c`, terrific", TEXT_TYPE.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TEXT_TYPE.CODE)
         expected_result = [
             TextNode("This is ", TEXT_TYPE.TEXT),
@@ -25,18 +25,32 @@ class TestUtils(unittest.TestCase):
             TextNode("code b", TEXT_TYPE.CODE),
             TextNode(" and this is ", TEXT_TYPE.TEXT),
             TextNode("code c", TEXT_TYPE.CODE),
-            TextNode(", tremendo", TEXT_TYPE.TEXT),
+            TextNode(", terrific", TEXT_TYPE.TEXT),
         ]
 
         self.assertListEqual(new_nodes, expected_result)
+
+    def test_splitter_bold(self):
+        node = TextNode("This is a text with **bold** text", TEXT_TYPE.BOLD)
+        new_nodes = split_nodes_delimiter([node], "**", TEXT_TYPE.BOLD)
+        expected_result = [
+            TextNode("This is a text with ", TEXT_TYPE.TEXT),
+            TextNode("bold", TEXT_TYPE.BOLD),
+            TextNode(" text", TEXT_TYPE.TEXT),
+        ]
+
+        self.assertListEqual(new_nodes, expected_result)
+
+    def test_splitter_malformed_md(self):
+        node = TextNode("This is a malformed md *bold is not closed", TEXT_TYPE.BOLD)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "*", TEXT_TYPE.BOLD)
 
     def test_splitter_no_delimiter(self):
         node = TextNode("This is text with a `code block` word", TEXT_TYPE.TEXT)
-        new_nodes = split_nodes_delimiter([node], "", TEXT_TYPE.CODE)
-        expected_result = [
-            TextNode("This is text with a `code block` word", TEXT_TYPE.CODE),
-        ]
-
-        self.assertListEqual(new_nodes, expected_result)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "", TEXT_TYPE.CODE)
 
 
+if __name__ == "__main__":
+    unittest.main()
