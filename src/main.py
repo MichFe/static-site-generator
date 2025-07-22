b5f2b0b1b876bea644b36a12d4f83c588d4ee039
Michelle Felix <mich.feloz@gmail.com>
2025-07-21 22:41:31 -0600

    feat(main): Adding the generate_page function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import os, shutil
 from classes.textnode import TEXT_TYPE, TextNode
+from utils.extract_title import extract_title
+from utils.markdown_to_html_node import markdown_to_html_node
 
 def main():
     shutil.rmtree('./public')
     os.mkdir('./public')
     copy_static('static', 'public')
+
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 def copy_static(origin, destination):
     print(f"origin: {origin}, destination: {destination}")
@@ -23,6 +27,27 @@ def copy_static(origin, destination):
 
         os.mkdir(out_path)
         copy_static(src_path, out_path)
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    md_file = open(from_path, 'r', encoding='utf-8')
+    md_content = md_file.read()
+    md_file.close()
+
+    template_file = open(template_path, 'r', encoding='utf-8')
+    template_content = template_file.read()
+
+    html_content = markdown_to_html_node(md_content).to_html()
+
+    page_title = extract_title(md_content)
+
+    template_content = template_content.replace("{{ Title }}", page_title)
+    template_content = template_content.replace("{{ Content }}", html_content)
+
+
+    html_file = open(dest_path, 'w', encoding='utf-8')
+    html_file.write(template_content)
+    html_file.close()
 
 
 if __name__ == "__main__":
